6d9ae1ee89305421c704507d82289d426fe0f65e
Henry Langmack <Henry.langmack@gmail.com>
2023-03-12 14:45:26 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 npm i minify@7.0.0 -g
 apt-get update
 apt-get -y install moreutils
-cd _site
+cd dist # assuming site build is in dist
 find . -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
     do
     minify ${fname} | sponge ${fname}
